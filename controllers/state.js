136aa798368c72276f8c85e1dfb1038a933d4e9c
Vaclav Klecanda <vencax77@gmail.com>
2014-10-22 00:52:43 +0200

    account object extra route

--- controllers/state.js
+++ controllers/state.js
@@ -3,9 +3,21 @@
 module.exports = function(models) {
   return {
 
+    findAccount: function(req, res, next) {
+      models.CreditAccount
+        .find({where: {uid: req.user.id}})
+        .on('success', function(account) {
+          if (account) {
+            req.account = account;
+            return next();
+          } else {
+            return res.status(404).send('account not found');
+          }
+        })
+    },
+
     curr: function(req, res, next) {
-      var h = {uid: 11, state: 200}
-      res.status(200).send(h);
+      res.status(200).send(req.account);
     },
 
     history: function(req, res, next) {
